34f593e8601ac287c4e311b0a23e4e4f919fcec9
Malik Abiola <abiola.malik@gmail.com>
2017-02-05 12:46:29 +0100

    Add tracking
    - added tracking to message body
    - made some css changes

--- JS/content-script.js
+++ JS/content-script.js
@@ -2,25 +2,46 @@
 var storageApi = chrome.storage;
 var areaOfInterest = '';
 var preferredWidth = 0;
+var preferredHeight = 0;
 
 $(document).ready(function () {
     var documentBody = $("body");
 
+    // when compose message/reply actions is shown
     $(document).arrive("tr.n1tfz", function() {
         areaOfInterest = $(this).children('td:first');
         updateView();
     });
 
+    // when message body is shown
+    $(document).arrive("div[role='textbox']", function() {
+        addTracker($(this));
+    });
+
+    // listen for updated change in extension settings.
     storageApi.onChanged.addListener(function () {
         updateView();
     });
 
-    documentBody.on('click', '#extension_schedule_btn', function() {
+    documentBody.on('click', '#extension_schedule_btn', function(e) {
+        e.preventDefault();
         scheduleButtonClickAction();
     });
 
-    documentBody.on('click', '#close_scheduler_view', function() {
+    documentBody.on('click', '#close_scheduler_view', function(e) {
+        e.preventDefault();
         $("#scheduler_view").hide();
+    });
+
+    documentBody.on('click', '#schedule_mail_btn', function(e) {
+        e.preventDefault();
+
+        var scheduleSuccessMessage = $("<div><h3>Mail Scheduled Successfully.</h3></div>");
+        scheduleSuccessMessage.css('width', preferredWidth);
+        scheduleSuccessMessage.css('min-height', preferredHeight);
+
+        $("#scheduler_view").find(".modal-content").html(scheduleSuccessMessage);
+        //@todo: close compose view
     });
 });
 
@@ -45,6 +66,7 @@ var scheduleButtonClickAction = function () {
 
     var schedulerView = $("#scheduler_view");
     schedulerView.css('width', preferredWidth);
+    schedulerView.css('min-height', preferredHeight);
     schedulerView.show();
 };
 
@@ -66,5 +88,17 @@ var loadMailScheduler = function () {
         var parent = areaOfInterest.closest('table.iN');
         parent.append(data);
         preferredWidth = parent.width();
+        preferredHeight = parent.height();
+    });
+};
+
+var addTracker = function (areaOfInterest)
+{
+    storageApi.sync.get({trackMessage: false}, function(item) {
+        if (item.trackMessage) {
+            if (! areaOfInterest.has('#extension_tracter').length) {
+                areaOfInterest.append($("<div id='extension_tracker'>some tracking image injected here.</div>"));
+            }
+        }
     });
 };
